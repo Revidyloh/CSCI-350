f217f60a800be33519972bd9d66a6046ccdf7f3b
Dan Cambitsis <dcam90@gmail.com>
2016-11-06 18:37:03 -0500

    Add files via upload

---
+++ baseline1actual.lisp
@@ -0,0 +1,12 @@
+(setf counter 0)
+(defun baseline1 (board colors SCSA last-response)
+  (declare (ignore SCSA last-response))
+  (let ((colors-copy colors) (guess '()))
+  (if (equal last-response nil) (progn (setf counter 1) (setf guess (make-list board :initial-element 'A)))
+  (progn (setf guesslist (increment colors-copy board)) (setf guess (nth counter guesslist)) (incf counter)))
+  guess))
+
+(defun increment (colors-copy board)
+  (if (= board 1) (loop for x in colors-copy collect (list x))
+    (mapcan #'(lambda (x) (mapcar #'(lambda (y) (cons x y))
+    (increment colors-copy (1- board)))) colors-copy)))
