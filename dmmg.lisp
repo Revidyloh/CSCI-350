0275ec6aa82562bb707ba26021d03b340adf11b4
Dan Cambitsis <dcam90@gmail.com>
2016-11-30 12:20:05 -0500

    Add files via upload

---
+++ dmmg.lisp
@@ -0,0 +1,8 @@
+(defparameter *counter* 0)
+(defparameter *guess* ())
+(defparameter *bull-check* 1)
+(defun DMMG (board colors SCSA last-response)
+  (when (equal SCSA 'ab-color) (ab-color-guesser board colors SCSA last-response))
+  (when (equal SCSA 'only-once) (only-once-guesser board colors SCSA last-response))
+  *guess*
+) 
